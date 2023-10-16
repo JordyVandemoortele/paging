fdf5612d5da308d8f42945d835dfc4a3908b0db9
JordyVandemoortele <125571637+JordyVandemoortele@users.noreply.github.com>
2023-10-16 15:51:45 +0200

    Initial commit

---
+++ Vives.Services.Model/PagedServiceResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Vives.Services.Model
+{
+    public class PagedServiceResult<T>: ServiceResult
+    {
+        public IList<T> Data { get; set; } = new List<T>();
+        public required Paging Paging { get; set; }
+        public int TotalCount { get; set; }
+    }
+}
